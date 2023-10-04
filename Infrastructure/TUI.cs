a304b92f590abb4e8465366df4e336622fd2afd0
Artiom-Evs <evs.a.s@mail.ru>
2023-10-04 15:09:19 +0200

    Edit Program class
    Logic of the game start and has been implemented.
    TUI class has also been implemented. This class contains all methods for work with console.

--- Infrastructure/TUI.cs
+++ Infrastructure/TUI.cs
@@ -1,4 +1,6 @@
 ﻿
+using RPSGame.Models;
+
 namespace RPSGame.Infrastructure;
 
 /// <summary>
@@ -6,18 +8,114 @@ namespace RPSGame.Infrastructure;
 /// </summary>
 internal static class TUI
 {
-    internal static void PrintStartError()
-    {
+    internal static void PrintZeroArgumentsError() =>
+        Console.WriteLine("The game can't start without arguments.\nBye!");
 
-    }
+    internal static void PrintNumberOfArgumentsLessThenThreeError() =>
+        Console.WriteLine("The number of start arguments cannot be less then 3.\nBye!");
+
+    internal static void PrintNumberOfArgumentsIsNotOddError() =>
+        Console.WriteLine("The number of start arguments must be odd.\nBye!");
+
 
     internal static void PrintGameStartMessage()
     {
-
+        Console.WriteLine("Welcome to Rock-Paper-Scissors like game!\n");
     }
 
-    internal static void PrintGameEndMessage()
+    internal static void PrintGameEndMessage(GameEndedEventArgs e)
     {
+        Console.WriteLine("Bye!");
+    }
 
+    internal static void PrintRoundStartMessage(RoundStartedEventArgs e)
+    {
+        Console.WriteLine($"Round #{e.RoundNumber}\n");
+    }
+
+    internal static void PrintRoundEndMessage(RoundEndedEventArgs e)
+    {
+        Console.WriteLine($"PC's choice: {e.PCMoveChoice}" +
+                          $"\nHMAC key: {e.PCSecretKey}\n");
+
+        switch (e.Result)
+        {
+            case RoundResults.Draw:
+                Console.WriteLine("Draw! Game continues!\n");
+                break;
+            case RoundResults.PCWin:
+                Console.WriteLine("PC won!");
+                break;
+            case RoundResults.PlayerWin:
+                Console.WriteLine("You won!");
+                break;
+            case RoundResults.PlayerCapitulate:
+                Console.WriteLine("You are capitulate! PC won.");
+                break;
+            default:
+                break;
+        }
+    }
+
+    internal static void PrintPCStartMessage()
+    {
+        Console.WriteLine("PC makes his move...");
+    }
+
+    internal static void PrintPCEndMessage(PCMoveEndedEventArgs e)
+    {
+        Console.WriteLine($"PC has made his move.\nHMAC: {e.ChoiceSignature}\n");
+    }
+
+    internal static void PrintPlayerMoveStartMessage()
+    {
+        Console.WriteLine("Your move!");
+    }
+
+    internal static void PrintPlayerMoveEndMessage(PlayerMoveEndedEventArgs e)
+    {
+        Console.WriteLine($"Your choice: {e.PlayerChoice}\n");
+    }
+
+    internal static int PrintPlayerMoveSelectionDialog(string[] moveVariants)
+    {
+        Console.WriteLine("Available moves:");
+
+        for (int i = 0; i <  moveVariants.Length; i++)
+        {
+            Console.WriteLine($"{i + 1} - {moveVariants[i]}");
+        }
+
+        Console.WriteLine($"{moveVariants.Length + 1} - Exit" +
+                          $"\n? - Help");
+
+        int choice = 0;
+
+        do
+        {
+            Console.Write("Make your choice: ");
+            string input = Console.ReadLine();
+
+            if (input == "?")
+            {
+                TUI.PrintHelpMessage(moveVariants);
+                choice = 0;
+                continue;
+            }
+            else if (!int.TryParse(input, out choice) ||
+                choice < 1 ||
+                choice > moveVariants.Length + 1)
+            {
+                Console.WriteLine("You entered something wrong!");
+                choice = 0;
+            }
+        } while (choice == 0);
+
+        return  choice - 1;
+    }
+
+    private static void PrintHelpMessage(string[] moveVariants)
+    {
+        throw new NotImplementedException();
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,9 +1,96 @@
-﻿namespace RPSGame;
+﻿using RPSGame.Infrastructure;
+using RPSGame.Services;
 
-internal class Program
+namespace RPSGame;
+
+internal static class Program
 {
+    static GameContext _game = default!;
+
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        if (args.Length == 0)
+        {
+            TUI.PrintZeroArgumentsError();
+            return;
+        }
+
+        if (args.Length < 3)
+        {
+            TUI.PrintNumberOfArgumentsLessThenThreeError();
+            return;
+        }
+
+        if (args.Length % 2 == 0)
+        {
+            TUI.PrintNumberOfArgumentsIsNotOddError();
+            return;
+        }
+
+        IKeyGenerator keyGenerator = new BaseKeyGenerator();
+        IDataSigner dataSigner = new HMACSHA2DataSigner(keyGenerator);
+
+        _game = new GameContext(dataSigner, args, PCMoveHandler, PlayerMoveHandler);
+
+        _game.GameStarted += Game_GameStarted;
+        _game.GameEnded += Game_GameEnded;
+        _game.RoundStarted += Game_RoundStarted;
+        _game.RoundEnded += Game_RoundEnded;
+        _game.PCMoveStarted += Game_PCMoveStarted;
+        _game.PCMoveEnded += Game_PCMoveEnded;
+        _game.PlayerMoveStarted += Game_PlayerMoveStarted;
+        _game.PlayerMoveEnded += Game_PlayerMoveEnded;
+
+        _game.StartGame();
+    }
+
+    private static int PCMoveHandler(string[] moveVariants)
+    {
+        return Random.Shared.Next(moveVariants.Length);
+    }
+
+    private static int PlayerMoveHandler(string[] moveVariants)
+    {
+        return TUI.PrintPlayerMoveSelectionDialog(moveVariants);
+    }
+
+    private static void Game_GameStarted(object? sender, Models.GameStartedEventArgs e)
+    {
+        TUI.PrintGameStartMessage();
+    }
+
+    private static void Game_GameEnded(object? sender, Models.GameEndedEventArgs e)
+    {
+        TUI.PrintGameEndMessage(e);
+    }
+
+    private static void Game_RoundStarted(object? sender, Models.RoundStartedEventArgs e)
+    {
+        TUI.PrintRoundStartMessage(e);
+    }
+
+    private static void Game_RoundEnded(object? sender, Models.RoundEndedEventArgs e)
+    {
+        TUI.PrintRoundEndMessage(e);
+    }
+
+    private static void Game_PCMoveStarted(object? sender, Models.PCMoveStartedEventArgs e)
+    {
+        TUI.PrintPCStartMessage();
+    }
+
+    private static void Game_PCMoveEnded(object? sender, Models.PCMoveEndedEventArgs e)
+    {
+        TUI.PrintPCEndMessage(e);
+    }
+
+    private static void Game_PlayerMoveStarted(object? sender, Models.PlayerMoveStartedEventArgs e)
+    {
+        TUI.PrintPlayerMoveStartMessage();
+    }
+
+    private static void Game_PlayerMoveEnded(object? sender, Models.PlayerMoveEndedEventArgs e)
+    {
+        TUI.PrintPlayerMoveEndMessage(e);
     }
 }
